6920af77228deb6772a86e4c835d9c1b671ebf93
Christian Kischy Haege <kischy@gmx.de>
2020-08-22 15:06:28 +0200

    Added starting project and test files

---
+++ p21-euler/p21-euler/p21-euler.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Calculation started" << endl;
+
+    int answer_p21 = 0;
+
+
+    cout << "The answer to problem 20 of project Euler is " << answer_p21 << "." << endl;
+}
+
